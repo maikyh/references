bb24b0282a9c72c1d6cc9c0f4cdfab8a3f645df0
maikyh <98727536+maikyh@users.noreply.github.com>
2022-03-27 13:46:35 -0600

    Update and rename Default Template to Default Template.cpp

--- Default Template.cpp
+++ Default Template.cpp
@@ -45,49 +45,6 @@ const int INF = INT_MAX;
 const int INFNEG = INT_MIN;
 const ll int MOD = 1e9+7;
 const double EP = 1E-10;
-
-vector<string> split (string s, string delimiter) {
-    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-    string token;
-    vector<string> res;
-
-    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
-        token = s.substr (pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
-        res.push_back (token);
-    }
-
-    res.push_back (s.substr (pos_start));
-    return res;
-}
-
-string& remove_chars(string& s, const string& chars) {
-    s.erase(remove_if(s.begin(), s.end(), [&chars](const char& c) {
-        return chars.find(c) != string::npos;
-    }), s.end());
-    return s;
-}
-
-string to_uppercase(string cadena) {
-  for (int i = 0; i < cadena.length(); i++) cadena[i] = toupper(cadena[i]);
-  return cadena;
-}
-
-string to_lowercase(string cadena) {
-  for (int i = 0; i < cadena.length(); i++) cadena[i] = tolower(cadena[i]);
-  return cadena;
-}
-
-string strip(const string &inpt) // " ABC" -> "ABC"
-{
-    auto start_it = inpt.begin();
-    auto end_it = inpt.rbegin();
-    while (std::isspace(*start_it))
-        ++start_it;
-    while (std::isspace(*end_it))
-        ++end_it;
-    return std::string(start_it, end_it.base());
-}
  
 // bool valid(int r, int c){
 //     return r>=0 && c>=0 && r<n && c<m;
