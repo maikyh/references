2188b2154b0f89bab263cd84d1d640352c9358bb
maikyh <98727536+maikyh@users.noreply.github.com>
2022-03-27 20:20:55 -0600

    Update HasCycle.cpp

--- HasCycle.cpp
+++ HasCycle.cpp
@@ -10,3 +10,22 @@ bool hasCycle(int me, vector<int> &state, vector<vector<int>> &graph){
     state[me] = 2;
     return sol;
 }
+void solve(){
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> graph(n);
+
+    for(int i=0; i<m; i++){
+        int x,y;
+        cin >> x >> y; x--; y--;
+        graph[x].pb(y);
+        graph[y].pb(x);
+    }
+
+    vector<int> state(n, 0);
+    bool ans = false;
+    for(int i=0; i<n; i++){
+        if(state[i] == 0) ans = ans || hasCycle(i, state, graph);
+    }
+    cout << (ans?"YES":"NO") << endl;
+}
