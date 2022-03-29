1662a87077172be8e30cc7966f782e8aec40bd5e
maikyh <98727536+maikyh@users.noreply.github.com>
2022-03-28 19:39:11 -0600

    Update Kruskal.cpp

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -7,50 +7,48 @@ struct Edge {
     }
 };
 
-struct UnionFind {
-    int numSets = 0;
-    int setSize[MAXN];
-    int parent[MAXN];
-    int rank[MAXN];
-
-    UnionFind(int n) {
-        numSets = n;
-        FOR(i, 0, n)    parent[i] = i;
+struct dsu{
+    static const int MAXN=2 * 1e5+1;
+    int P[MAXN], R[MAXN];
+    int _size;
+    void init(int n){ // init
+        static int i;
+        for(int i = 0; i <= n; i++)
+            P[i] = i, R[i] = 1;
+        _size = n;
     }
 
-    void make_set(int i) {
-        parent[i] = i;
-        rank[i] = 0;
+    int get(int x){ // find o root   get
+        if (P[x] != x) P[x] = get(P[x]);
+        return P[x];
     }
 
-    int find_set(int i) {
-        if (i != parent[i])
-            parent[i] = find_set(parent[i]);
-        return parent[i];
-    }
-
-    void unionSet(int i, int j) {
-        if (!isSameSet(i, j)) {
-            numSets--;
-            int x = find_set(i), y = find_set(j);
-
-            if (rank[x] > rank[y]) {
-                parent[y] = x;
-                setSize[x] += setSize[y];
+    int unify(int x, int y) { //joint o unite   unify
+        x = get(x), y = get(y);
+        if(x != y) {
+            _size--;
+            if(R[x] > R[y]){
+                R[x] += R[y], P[y] = x;
+                // cout<<(R[x])<<'\n';
             }
-            else {
-                parent[x] = y;
-                setSize[y] += setSize[x];
-
-                if (rank[x] == rank[y])     rank[y]++;
+            else{
+                R[y] += R[x], P[x] = y;
+                // cout<<(R[y])<<'\n';
             }
+            return 1;
         }
+        // cout<<(R[x])<<'\n';
+        return 0;
     }
 
-    bool isSameSet(int i, int j) {
-        return find_set(i) == find_set(j);
+    bool same(int a,int b){
+        return get(a)==get(b);
     }
-};
+
+    int getSize() {
+        return _size;
+    }
+} dsu;
 
 vector<Edge> v;
 vector<Edge> tree;
@@ -59,14 +57,14 @@ int kruskal(int n, int m) { // O(ELogE + ELogV)
     sort(v.begin(), v.end());
 
     int mst_w = 0;
-    UnionFind UF(n);
+    dsu.init(n);
 
     FOR(i, 0, m) {
         Edge e = v[i];
 
-        if (! UF.isSameSet(e.from, e.to)) {
+        if (! dsu.same(e.from, e.to)) {
             mst_w += e.w;
-            UF.unionSet(e.from, e.to);
+            dsu.unify(e.from, e.to);
             tree.pb(Edge(e.from, e.to, e.w));
         }
     }
