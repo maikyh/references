045b7106cfae6c01c7f05bf4ab46affd78fdb1ea
Miguel Garza <98727536+maikyh@users.noreply.github.com>
2022-04-15 01:37:19 -0500

    Update SegTree.cpp

--- SegTree.cpp
+++ SegTree.cpp
@@ -1,69 +1,78 @@
-template <typename num_t> 
-struct segtree {
-  int n, depth;
-  vector<num_t> tree, lazy;
-
-  void init(int s, long long* arr) {
-    n = s;
-    tree = vector<num_t>(4 * s, 0);
-    lazy = vector<num_t>(4 * s, 0);
-    init(0, 0, n - 1, arr);
-  }
-
-  num_t init(int i, int l, int r, long long* arr) {
-    if (l == r) return tree[i] = arr[l];
-
-    int mid = (l + r) / 2;
-    num_t a = init(2 * i + 1, l, mid, arr),
-          b = init(2 * i + 2, mid + 1, r, arr);
-    return tree[i] = a.op(b);
-  }
-
-  void update(int l, int r, num_t v) {
-	if (l > r) return;
-    update(0, 0, n - 1, l, r, v);
-  }
-
-  num_t update(int i, int tl, int tr, int ql, int qr, num_t v) {
-    eval_lazy(i, tl, tr);
-	
-	if (tr < ql || qr < tl) return tree[i];
-    if (ql <= tl && tr <= qr) {
-      lazy[i] = lazy[i].val + v.val;
-      eval_lazy(i, tl, tr);
-      return tree[i];
+const int mxN = 2e5;
+template<class T> struct Seg {
+    int n;
+    T ID = 0;
+    vector<T> seg;
+    T comb (T a, T b) {return a + b;}
+    void init (int _n) {n = _n; seg.assign(n * 2, ID);}
+    void pull (int p) { // SUM;
+        seg[p] = comb(seg[p * 2], seg[p * 2 + 1]);
     }
-    
-    int mid = (tl + tr) / 2;
-    num_t a = update(2 * i + 1, tl, mid, ql, qr, v),
-          b = update(2 * i + 2, mid + 1, tr, ql, qr, v);
-    return tree[i] = a.op(b);
-  }
-
-  num_t query(int l, int r) {
-	if (l > r) return num_t::null_v;
-    return query(0, 0, n-1, l, r);
-  }
-
-  num_t query(int i, int tl, int tr, int ql, int qr) {
-    eval_lazy(i, tl, tr);
-    
-    if (ql <= tl && tr <= qr) return tree[i];
-    if (tr < ql || qr < tl) return num_t::null_v;
-
-    int mid = (tl + tr) / 2;
-    num_t a = query(2 * i + 1, tl, mid, ql, qr),
-          b = query(2 * i + 2, mid + 1, tr, ql, qr);
-    return a.op(b);
-  }
-
-  void eval_lazy(int i, int l, int r) {
-    tree[i] = tree[i].lazy_op(lazy[i], (r - l + 1));
-    if (l != r) {
-      lazy[i * 2 + 1] = lazy[i].val + lazy[i * 2 + 1].val;
-      lazy[i * 2 + 2] = lazy[i].val + lazy[i * 2 + 2].val;
+    void upd (int i, T val) { // SUM; sgt.upd(i,x[i]); (ll x = sgt.seg[a-1+n] | sgt.upd(a-1,b-x)); update the value at position k to  u; what is the sum of values in range [a,b]?
+        seg[i+=n] += val;
+        for (i /= 2; i; i /= 2) {
+            pull(i);
+        }
     }
-
-    lazy[i] = num_t();
-  }
+    ll sum(int a, int b){ // SUM;
+        a+=n, b+=n;
+        ll s = 0;
+        while(a<=b){
+            if (a&1) s+= seg[a++]; //a is right child
+            if (~b&1) s+= seg[b--]; // b is left child
+            a>>=1, b>>=1;
+        }
+        return s;
+    }
+    void updmn(int k, int x){ // MIN; sgt.upd(i,x[i]); sgt.upd(a-1,b); update the value at position k to u; what is the minimum value in range [a,b]?
+        k+=n;
+        seg[k] = x; k>>=1;
+        while(k>=1){
+            seg[k]  = min(seg[2*k], seg[2*k+1]);
+            k>>=1;
+        }
+    }
+    ll mn(int a, int b){ // MIN;
+        a+=n, b+=n;
+        ll s = INF;
+        while(a<=b){
+            if (a&1) s = min(s,seg[a++]); //a is right child
+            if (~b&1) s = min(s,seg[b--]); // b is left child
+            a>>=1, b>>=1;
+        }
+        return s;
+    } 
 };
+
+//Range Update Queries
+///////////////////START HERE////////////////////////
+void solve(){
+    int n,q;
+    cin >> n >> q;
+ 
+    Seg<ll> sgt;
+ 
+    sgt.init(n+1);
+    vi a(n+1);
+    FOR(i,1,n+1){
+        cin >> a[i];
+        sgt.upd(i,a[i] - a[i-1]);
+    }
+ 
+    FOR(i,0,q){
+        int num;
+        cin >> num;
+        if(num==2){
+            int v;
+            cin >> v;
+            cout<<sgt.sum(1,v)<<'\n';
+        }
+        else{
+            ll x,y,u;
+            cin >> x >> y >> u;
+            sgt.upd(x,u);
+            sgt.upd(y+1,-u);
+        }
+    }
+}         
+/////////////////END HERE//////////////////////////////
