be1618057f17a2ffae80411166db090b490f2898
Miguel Garza <98727536+maikyh@users.noreply.github.com>
2022-08-20 23:04:02 -0500

    Update HasCycle.cpp

--- Graphs/HasCycle.cpp
+++ Graphs/HasCycle.cpp
@@ -1,31 +1,32 @@
-bool hasCycle(int me, vector<int> &state, vector<vector<int>> &graph){
+bool hasCycleUtil(int me, vector<int> &state, vector<vector<int>> &graph){
     if(state[me] == 1) return true;
     if(state[me] == 2) return false;
     state[me] = 1;
 
     bool sol = false;
     for(auto child:graph[me]){
-        sol = sol || hasCycle(child, state, graph);
+        sol = sol || hasCycleUtil(child, state, graph);
     }
     state[me] = 2;
     return sol;
+}   
+bool hasCycle(vector<vector<int>>& graph, int n){
+    vector<int> state(n, 0);
+    bool ans = false;
+    for(int i=0; i<n; i++){
+        if(state[i] == 0) ans = ans || hasCycleUtil(i, state, graph);
+    }
+    return ans;
 }
 void solve(){
     int n,m;
     cin >> n >> m;
-    vector<vector<int>> graph(n);
-
-    for(int i=0; i<m; i++){
-        int x,y;
-        cin >> x >> y; x--; y--;
-        graph[x].pb(y);
-        graph[y].pb(x);
+    vt<vi> adj(n);
+    while(m--){
+        int u,v;
+        cin >> u >> v;
+        u--; v--;
+        adj[u].pb(v);
     }
-
-    vector<int> state(n, 0);
-    bool ans = false;
-    for(int i=0; i<n; i++){
-        if(state[i] == 0) ans = ans || hasCycle(i, state, graph);
-    }
-    cout << (ans?"YES":"NO") << endl;
-}
+    cout<<(hasCycle(adj,n) ? "YES" : "NO")<<'\n';
+}  
