69f61cb30e4383db6734f5f09836c90528a2de85
Miguel Garza <98727536+maikyh@users.noreply.github.com>
2022-09-09 19:08:10 -0500

    Update Dijkstra.cpp

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -1,65 +1,56 @@
-const int mxN = 101;
-int vis[100005], dis[100005];
-vpi adj[100005];
-int vis2[100005], dis2[100005];
-vpi adj2[100005];
-void solve(){
-    int n,m; 
-    while(cin >> n >> m){
-        clr(vis,0);
-        clr(dis,0);
-        clr(adj,0);
-        clr(vis2,0);
-        clr(dis2,0);
-        clr(adj2,0);
-        FOR(i,0,m){
-            int u,v,num,w; 
-            cin >> u >> v >> num >> w;
-            if(num == 0)
-                adj[u].pb({v,w});
-            else
-                adj2[u].pb({v,w});
-        }
+const int mxN = 1e5 + 10;
+vpl adj1[mxN];
+ll dist1[mxN], par1[mxN];
 
-        //dijkstra 1
-        FOR(i,2,n+1) dis[i]=INF;
-        priority_queue<pi,vpi,greater<pi>> q;
-        q.push({0,1});
-        while(!q.empty()){
-            int u = q.top().se; q.pop();
-            if (vis[u]) continue;
-            vis[u]=1;
-            for (auto i : adj[u]){
-                int v = i.fi;
-                int w = i.se;
-                if (dis[v]>dis[u]+w){
-                    dis[v] = dis[u]+w;
-                    q.push({dis[v],v});
-                }
+vpl adj2[mxN];
+ll dist2[mxN], par2[mxN];
+
+bool vis[mxN];
+
+void dijkstra(int s, int t, ll dist[], vpl adj[], ll par[]){ // int n, int source, int to
+    priority_queue<pl, vector<pl>, greater<pl>> pq;
+    FOR(i,0,mxN) dist[i] = 1e18;
+    clr(vis,0);
+    pq.push(pi(0, s));
+    dist[s] = 0;
+    par[s] = -1;
+
+    while(!pq.empty()){
+        ll u = pq.top().se; pq.pop();
+        if(vis[u]) continue;
+        vis[u]=1;
+        for (auto &neigh: adj[u]){
+            ll v = neigh.first, w = neigh.second;
+            if (dist[u]+w < dist[v]){
+                dist[v] = dist[u]+w;
+                pq.push({dist[v],v});
+                par[v] = u;
             }
         }
-        // FOR(i,1,n+1) cout<<(dis[i])<<" ";
-
-        // cout<<'\n';
-
-        //dijkstra 2
-        FOR(i,2,n+1) dis2[i]=INF;
-        priority_queue<pi,vpi,greater<pi>> q2;
-        q2.push({0,1});
-        while(!q2.empty()){
-            int u = q2.top().se; q2.pop();
-            if (vis2[u]) continue;
-            vis2[u]=1;
-            for (auto i : adj2[u]){
-                int v = i.fi;
-                int w = i.se;
-                if (dis2[v]>dis2[u]+w){
-                    dis2[v] = dis2[u]+w;
-                    q2.push({dis2[v],v});
-                }
-            }
-        }
-        // FOR(i,1,n+1) cout<<(dis2[i])<<" ";
-            cout<<min(dis[n],dis2[n])<<'\n';
     }
-}     
+}
+void solve() {
+    ll n,m; cin >> n >> m;
+    vt<array<ll,3>> edges;
+    while(m--) {
+        ll u,v,w; cin >> u >> v >> w;
+        edges.pb({u,v,w});
+        adj1[u].push_back(pi(v, w));
+        adj2[v].push_back(pi(u, w));
+    }
+
+    ll mini = 1e18;
+    dijkstra(1,n,dist1,adj1,par1);
+    dijkstra(n,1,dist2,adj2,par2);
+    FORE(edge,edges) {
+        ll u = edge[0];
+        ll v = edge[1];
+        ll w = edge[2];
+        ll curr = dist1[u] + dist2[v] + w/2;
+        umin(mini,curr);
+    }
+
+    // FOR(i,1,n+1) cout<<(dist1[i])<<" ";
+    // FOR(i,1,n+1) cout<<(dist2[i])<<" ";
+    cout<<mini<<'\n';
+}   
