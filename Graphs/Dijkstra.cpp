39afd5d69f75f3de6677b3edbd5fa0a9b3a47616
Miguel Garza <98727536+maikyh@users.noreply.github.com>
2022-09-11 00:29:53 -0500

    Update Dijkstra.cpp

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -2,10 +2,8 @@ const int mxN = 1e5 + 10;
 vpl adj1[mxN];
 ll dist1[mxN], par1[mxN];
 
-vpl adj2[mxN];
-ll dist2[mxN], par2[mxN];
-
 bool vis[mxN];
+bool reached = 0;
 
 void dijkstra(int s, int t, ll dist[], vpl adj[], ll par[]){ // int n, int source, int to
     priority_queue<pl, vector<pl>, greater<pl>> pq;
@@ -17,6 +15,7 @@ void dijkstra(int s, int t, ll dist[], vpl adj[], ll par[]){ // int n, int sourc
 
     while(!pq.empty()){
         ll u = pq.top().se; pq.pop();
+        if(u == t) reached = 1;
         if(vis[u]) continue;
         vis[u]=1;
         for (auto &neigh: adj[u]){
@@ -29,28 +28,34 @@ void dijkstra(int s, int t, ll dist[], vpl adj[], ll par[]){ // int n, int sourc
         }
     }
 }
+vi restorePath(int t){
+    int n = t;
+
+    vi path;
+    for(int v = n; v != -1; v = par1[v]) 
+        path.pb(v);
+    
+    return path;
+}
+
 void solve() {
     ll n,m; cin >> n >> m;
-    vt<array<ll,3>> edges;
     while(m--) {
         ll u,v,w; cin >> u >> v >> w;
-        edges.pb({u,v,w});
         adj1[u].push_back(pi(v, w));
-        adj2[v].push_back(pi(u, w));
+        adj1[v].pb(pi(u,w));
     }
 
-    ll mini = 1e18;
     dijkstra(1,n,dist1,adj1,par1);
-    dijkstra(n,1,dist2,adj2,par2);
-    FORE(edge,edges) {
-        ll u = edge[0];
-        ll v = edge[1];
-        ll w = edge[2];
-        ll curr = dist1[u] + dist2[v] + w/2;
-        umin(mini,curr);
+
+    if(!reached){
+        cout<<-1<<'\n';
+        return;
     }
 
-    // FOR(i,1,n+1) cout<<(dist1[i])<<" ";
-    // FOR(i,1,n+1) cout<<(dist2[i])<<" ";
-    cout<<mini<<'\n';
-}   
+    vi path = restorePath(n);
+    ROF(i,0,path.size())
+        cout<<path[i]<<" ";
+
+    // FOR(i,1,n+1) cout<<(dist[i])<<" ";
+}
