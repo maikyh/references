be69061a66c7d79a000f43b4c63a93929c1a5241
Miguel Garza <98727536+maikyh@users.noreply.github.com>
2022-12-28 18:56:15 -0600

    Create prefix xor-s.cpp

---
+++ Extras/prefix xor-s.cpp
@@ -0,0 +1,29 @@
+//total number of subarrays having a given XOR k
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int solve(vector<int> &A, int B) {
+    unordered_map<int,int>visited;
+    int cpx = 0;
+    long long c=0;
+    for(int i=0;i<A.size();i++){
+        cpx^=A[i];
+        if(cpx==B) c++;
+        int h = cpx^B;
+        if(visited.find(h)!=visited.end()){
+            c=c+visited[h];
+        }
+        visited[cpx]++;
+    }
+    return c;
+}
+};
+
+
+int main()
+{   vector<int> A{ 4,2,2,6,4 };
+    Solution obj;
+    int totalCount= obj.solve(A,6);
+    cout<<"The total number of subarrays having a given XOR k is "<<totalCount<<endl;
+}
