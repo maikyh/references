8ff93b74dd3ddf4e774caac2f87a64b07ea5615a
Miguel Garza <98727536+maikyh@users.noreply.github.com>
2023-05-07 17:45:31 -0600

    Update prefix xor-s.cpp

--- Extras/prefix xor-s.cpp
+++ Extras/prefix xor-s.cpp
@@ -28,3 +28,28 @@ int main()
     int totalCount= obj.solve(A,6);
     cout<<"The total number of subarrays having a given XOR k is "<<totalCount<<endl;
 }
+
+
+//total number of subsubsequences having a given XOR k mod 1e9+7
+void solve(){
+    int n, x; cin >> n >> x;
+    vector<int> a(n + 1);
+    vector<vector<int>> dp(n + 1, vector<int>(1 << 6, 0));
+    for(int i = 1; i <= n; ++i) {
+        cin >> a[i];
+        for(int mask = 0; mask < (1 << 6); ++mask) {
+            dp[i][mask] += dp[i - 1][mask];
+            if(dp[i][mask] >= MOD) dp[i][mask] -= MOD;
+            dp[i][mask & a[i]] += dp[i - 1][mask];
+            if(dp[i][mask & a[i]] >= MOD) dp[i][mask & a[i]] -= MOD;
+        }
+        dp[i][a[i]] = (dp[i][a[i]] + 1) % MOD;
+    }
+    int ans = 0;
+    for(int mask = 0; mask < (1 << 6); ++mask) {
+        if(__builtin_popcount(mask) == x) {
+            ans = (ans + dp[n][mask]) % MOD;
+        }
+    }
+    cout << ans << "\n";
+}    
