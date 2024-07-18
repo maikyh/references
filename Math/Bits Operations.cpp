46a8e46467680c2387d6064218f0d8d2b38f29d1
linusmtz <84740505+linusmtz@users.noreply.github.com>
2024-07-17 23:01:07 -0600

    Update Bits Operations.cpp

--- Math/Bits Operations.cpp
+++ Math/Bits Operations.cpp
@@ -22,6 +22,17 @@ int getSetBitsFromOneToN(int N)
     } 
     return ans; 
 } 
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
 
 void setBitsInArray(ll n, vector<int> &bits) {
     int position = 1;
